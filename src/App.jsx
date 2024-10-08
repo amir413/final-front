3a080cf1ae0e5bd505e0d265b4356131a54cc685
amir413 <amir.emad2007@gmail.com>
2024-10-09 01:03:12 +0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import React from 'react';
+import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
-import Footer from './components/Footer'; // Ensure consistent naming
-import Navbar from './components/Navbar'; // Ensure consistent naming
+import Footer from './components/footer';
+import Navbar from './components/navbar';
 import Women from './components/Women';
 import Men from './components/Men';
 import Children from './components/Children';
 import ItemDetail from './components/ItemDetail';
-
+import axios from 'axios'
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <Router>
       <Navbar />
@@ -19,6 +22,7 @@ function App() {
         <Route path="/men" element={<Men />} />
         <Route path="/children" element={<Children />} />
       </Routes>
+
       <Footer />
     </Router>
   );
